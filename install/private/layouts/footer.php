1fc847f634c0414177fe321cc476ea2f22aa82de
NIN1275 <106284474+nin1275@users.noreply.github.com>
2023-09-17 14:14:29 -0400

    Delete install directory

--- install/private/layouts/footer.php
+++
@@ -1,3 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.actual/1.0.19/jquery.actual.min.js"></script>
-<script src="/apps/install/public/assets/js/main.min.js"></script>
